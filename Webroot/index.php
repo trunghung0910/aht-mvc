2eda56ef25de280559ac82c63f7c5f2c8bccd3ee
unknown <zztrunghungzz@gmail.com>
2021-11-18 16:08:13 +0700

    chuyen sang chuan psr-4

---
+++ Webroot/index.php
@@ -0,0 +1,20 @@
+<?php
+
+define('WEBROOT', str_replace("Webroot/index.php", "", $_SERVER["SCRIPT_NAME"]));
+define('ROOT', str_replace("Webroot/index.php", "", $_SERVER["SCRIPT_FILENAME"]). 'src/');
+define('BASEPATH', str_replace("Webroot/index.php", "", $_SERVER["SCRIPT_FILENAME"]));
+
+//require(ROOT . 'Config/core.php');
+//
+//require(ROOT . 'router.php');
+//require(ROOT . 'request.php');
+//require(ROOT . 'dispatcher.php');
+
+require BASEPATH . '/vendor/autoload.php';
+
+use MVC\Dispatcher;
+
+$dispatch = new Dispatcher();
+$dispatch->dispatch();
+
+?>
